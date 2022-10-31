1b94499bed3ddec021aee8b5b405f7607387ec2f
Ashton Harvey <Ashtonh36@gmail.com>
2022-10-31 14:51:59 -0400

    basic UI finished. starting on js functionality

---
+++ src/js/Main.js
@@ -0,0 +1,22 @@
+//WORK IN PROGRESS!
+
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
+let cleanedOutput = document.getElementById("cleaned-output");
+let reversedOutput = document.getElementById("reversed-output");
+let output = document.getElementById("output");
+
+input.value = "";
+
+// submit.onclick = () => {
+//   let regex = /[^ -/:-@[-``]/gi;
+//   let extracted = input.value.match(regex);
+//   let cleanString = "";
+//   let splitString = "";
+//   let reversedString = "";
+
+// };
+
+function handleSubmit() {
+  console.log("hit submit button");
+}
